18841885bf4100302bd7a3ffe8cfd598f27b775a
jeromeshan <avshan@edu.hse.ru>
2022-01-09 02:54:47 +0300

    change grow function 0.77 on toy dataset

--- build/lib/mimaslib/cluster.py
+++ build/lib/mimaslib/cluster.py
@@ -3,73 +3,106 @@ import vg
 from pytransform3d.rotations import matrix_from_axis_angle
 from scipy.spatial.transform import Rotation as Rot
 from numpy import transpose, array, ndarray, hstack
+import numpy as np
 
+def n_dim_cube(n, init_length, init_width):
+    unit = [[ -0.5, -0.5, 0.5, 0.5], [ -0.5, 0.5, 0.5, -0.5]]
+
+    if n != 2:
+        for k in range(n-2):
+            l = len(unit[0])
+            for i in range(len(unit)):
+                unit[i] = unit[i]+unit[i]
+            unit.append( [-0.5]*l + [0.5]*l )
+    
+    for l in range(len(unit)):
+        if l == 0:
+            unit[l] = [element * init_length for element in unit[l]]
+        else:
+            unit[l] = [element * init_width for element in unit[l]]
+
+    return transpose(array(unit))
 
 class Cluster():
     # non shifted shape, easy to expand
-    non_rotated_cube= None
-    rotated_cube=None# shifted coor of rotated cube
-    centroid=None# coor of center
-    galaxies=None
+    non_rotated_cube = None
+    rotated_cube = None# shifted coor of rotated cube
+    centroid = None# coor of center
+    galaxies = None
+    grow_limit = None
+    new_galaxiex_koef = None
 
-    is_complete=False
-    times_grow=0
+    n_dim = 0
+
+    is_complete = False
+    times_grow = 0
     '''
     center - coordinates of center of cluster (in case on sigle galaxy coordinates on galaxy)
     non_rotated_cube - coordinates of non rotated parallelepiped that fits to cluster
     galaxies - coordinates on galaxies
     init_length - ration on longer part of parallelepiped to shorters
     '''
-    def __init__(self, center,non_rotated_cube=None,galaxies=None, init_length=0.5, init_width = 0.05):
+    def __init__(self, center, non_rotated_cube = None, galaxies = None, init_length = 0.5, init_width = 0.05, n_dim = 3, grow_limit = 5, new_galaxiex_koef = 1):
         
-        l_1 = [0, 0, 1, 1, 0, 0, 1, 1]
-        l_2 = [0, 1, 1, 0, 0, 1, 1, 0]
-        l_3 = [0, 0, 0, 0, 1, 1, 1, 1]
-        self.non_rotated_cube = (transpose(array([[element * init_width for element in l_1],[element * init_width for element in l_2],
-                                                  [element * init_length for element in l_3]]))-array([0.5*init_width,0.5*init_width,0.5*init_length]))
+        self.n_dim = n_dim 
+        self.grow_limit = grow_limit
+        self.new_galaxiex_koef = new_galaxiex_koef
+        self.non_rotated_cube = n_dim_cube(self.n_dim, init_length, init_width)
         
         if(isinstance(non_rotated_cube, ndarray)):
-            self.non_rotated_cube=non_rotated_cube
+            self.non_rotated_cube = non_rotated_cube
         if(not isinstance(galaxies, ndarray)):
-            self.galaxies=array([center])
+            self.galaxies = array([center])
         else:
-            self.galaxies=array(galaxies)
+            self.galaxies = array(galaxies)
         
-        self.centroid=center[:3]
-        self.rotated_cube = self.rotate(self.centroid,self.non_rotated_cube)+self.centroid
+        self.centroid = center[:self.n_dim]
+        self.rotated_cube = self.rotate(self.centroid, self.non_rotated_cube)+self.centroid
         
         for gal in self.galaxies:
-            if(Delaunay(self.rotated_cube).find_simplex(gal[:3]) < 0):
+            if(Delaunay(self.rotated_cube).find_simplex(gal[:self.n_dim]) < 0):
                 raise ValueError
         
     def get_length(self):
-        length  =  self.non_rotated_cube[4,2] - self.non_rotated_cube[0,2]
+        length  =  self.non_rotated_cube[2, 0] - self.non_rotated_cube[0, 0]
         return length
 
 
     def get_width(self):
-        width =  self.non_rotated_cube[2,0] - self.non_rotated_cube[0,0]
+        width =  self.non_rotated_cube[1, 1] - self.non_rotated_cube[0, 0]
         return width
 
         
     def rotate(self, vector, points):    
-        vector = vg.normalize(vector)
-        axis = vg.perpendicular(vg.basis.z, vector)
-        angle = vg.angle(vg.basis.z, vector, units='rad')
-        
-        a = hstack((axis, (angle,)))
-        R = matrix_from_axis_angle(a)
-        
-        r = Rot.from_matrix(R)
-        rotmat = r.apply(points)
-        
-        return rotmat
 
-    def grow(self,coef):
-        if(self.times_grow==5):
-            self.is_complete=True
+        x= array([1]+[0]*(self.n_dim-1))[np.newaxis, :].T
+        y= array(vector)[np.newaxis, :].T
+        y = y/np.linalg.norm(y)
+
+        u = x/np.linalg.norm(x)
+        v = y-(u.T@y)*u
+        v = v/np.linalg.norm(v)
+
+        cost = (x.T@y/np.linalg.norm(x)/np.linalg.norm(y))[0][0]
+        sint = np.sqrt(1-np.power(cost,2))
+
+        R = np.eye(len(x)) - u.dot(u.T) - v.dot(v.T)+(np.concatenate([u,v],axis = 1)@np.array([[cost, -sint],[sint,cost]]))@np.concatenate([u,v],axis = 1).T
+        
+        rotmat = []
+        for p in points:
+            rotmat.append(R@p)
+
+        return array(rotmat)
+
+    def grow(self, coef):
+        if(self.times_grow == self.grow_limit):
+            self.is_complete = True
             return
-        self.times_grow+=1
-        self.non_rotated_cube=self.non_rotated_cube*coef
-        self.rotated_cube = self.rotate(self.centroid,self.non_rotated_cube)+self.centroid         
+        self.times_grow += 1
+
+        composite_koef = self.new_galaxiex_koef * (1 + 1/np.power(len(self.galaxies),2))
+
+        self.new_galaxiex_koef = 1
+        self.non_rotated_cube = self.non_rotated_cube*composite_koef
+        self.rotated_cube = self.rotate(self.centroid, self.non_rotated_cube)+self.centroid         
     
--- build/lib/mimaslib/clusterer.py
+++ build/lib/mimaslib/clusterer.py
@@ -4,15 +4,16 @@ import ray
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 import time
-from numpy import transpose,array,arange,concatenate,eye,unique,dot,zeros,append,ones
+from numpy import transpose, array, arange, concatenate, eye, unique, dot, zeros, append, ones
 import numpy as np
 from numpy.linalg import norm
-from .cluster import Cluster
-# from fancyplot import *
+# from .cluster import Cluster, n_dim_cube
+from cluster import Cluster, n_dim_cube
+
 
 class Clusterer:
  
-    unit_cube=(transpose(array([[0, 0, 1, 1, 0, 0, 1, 1],[0, 1, 1, 0, 0, 1, 1, 0],[0, 0, 0, 0, 1, 1, 1, 1]]))-0.5)
+    unit_cube = (transpose(array([[0, 0, 1, 1, 0, 0, 1, 1], [0, 1, 1, 0, 0, 1, 1, 0], [0, 0, 0, 0, 1, 1, 1, 1]]))-0.5)
     init_cluster_length = None
     init_cluster_width = None
     coefs = 0
@@ -21,8 +22,8 @@ class Clusterer:
         init_length - ration on longer part of parallelepiped to shorters
         coefs - array of coefs to steps of algo
     '''
-    def __init__(self,init_cluster_length=0.5,init_cluster_width=0.05, coefs = 1+1/arange(1,100)[15:]*5, max_iter = 30, limit_radian = 0.01):
-        self.clusters=[]
+    def __init__(self, init_cluster_length = 0.5, init_cluster_width = 0.05, coefs = 1+1/arange(1, 100)[15:]*5, max_iter = 30, limit_radian = 0.01):
+        self.clusters = []
         self.init_cluster_length = init_cluster_length
         self.init_cluster_width = init_cluster_width
         self.coefs = coefs
@@ -31,174 +32,174 @@ class Clusterer:
  
     def merge(self):
  
-        clusters_len=len(self.clusters)
+        clusters_len = len(self.clusters)
  
  
         clusters_id = ray.put(self.clusters)
- 
-        graph=ray.get([self.parallel_connectivity_matrix.remote(self,clusters_id, subset) for subset in self.split_array(arange(len(self.clusters)),4)])
-        graph = concatenate( graph, axis=0 )
+        graph = ray.get([self.parallel_connectivity_matrix.remote(self, clusters_id, subset) for subset in self.split_array(arange(len(self.clusters)), 4)])
+        graph = concatenate( graph, axis = 0 )
 
         # graph = self.connectivity_matrix(self.clusters, arange(len(self.clusters)))
 
         graph = graph+transpose(graph)
-        graph=graph-eye(len(self.clusters))
+        graph = graph-eye(len(self.clusters))
  
         graph = csr_matrix(graph)
-        n_components, labels = connected_components(csgraph=graph, directed=False, return_labels=True)
+        n_components, labels = connected_components(csgraph = graph, directed = False, return_labels = True)
  
-        components=[]
+        components = []
  
-        np_clusters=array(self.clusters)
+        np_clusters = array(self.clusters)
  
-        components=[np_clusters[labels==label]  for label in unique(labels) ]
+        components = [np_clusters[labels == label]  for label in unique(labels) ]
  
-        new_clusters=list(map(self.collide_clusters,components))
+        new_clusters = list(map(self.collide_clusters, components))
  
-        self.clusters=new_clusters        
+        self.clusters = new_clusters        
  
- 
-    def check_collision(self,a:Cluster, b: Cluster):      
-        i=a.rotated_cube[1]-a.rotated_cube[0]
-        j=a.rotated_cube[3]-a.rotated_cube[0]
-        k=a.rotated_cube[4]-a.rotated_cube[0]
+    
+    def check_collision(self, a:Cluster, b: Cluster):
+
+        n = a.n_dim-2
+        bases = []
+        bases.append(a.rotated_cube[1]-a.rotated_cube[0])
+        bases.append(a.rotated_cube[3]-a.rotated_cube[0])
+        for i in range(n):
+            bases.append(a.rotated_cube[2**(i+2)]-a.rotated_cube[0])
         for point in b.rotated_cube:
-            u=point-a.rotated_cube[0]
-            u_i,u_j,u_k=dot(u,i),dot(u,j),dot(u,k)
-            if((0<= u_i)and(0<= u_j)and(0<= u_k)and
-               (u_i <= dot(i,i))and(u_j <= dot(j,j))and(u_k <= dot(k,k))):
-                return True
- 
-        i=b.rotated_cube[1]-b.rotated_cube[0]
-        j=b.rotated_cube[3]-b.rotated_cube[0]
-        k=b.rotated_cube[4]-b.rotated_cube[0]
-        for point in a.rotated_cube:
-            u=point-b.rotated_cube[0]
-            u_i,u_j,u_k=dot(u,i),dot(u,j),dot(u,k)
-            if((0<= u_i)and(0<= u_j)and(0<= u_k)and
-               (u_i <= dot(i,i))and(u_j <= dot(j,j))and(u_k <= dot(k,k))):
+            u = point-a.rotated_cube[0]
+            if ( all((0 <= dot(u, v)) and (dot(u, v) <= dot(v, v)) for v in bases)):
                 return True
         return False
  
     def collide_clusters(self, clusters:List[Cluster]):
  
-        if(len(clusters)==1):
+        if(len(clusters) == 1):
             return clusters[0]
-        galaxies=[cluster.galaxies for cluster in clusters]
-        vertex_points=[cluster.rotated_cube for cluster in clusters]
+
+        max_cluster_size = len(max(clusters, key = lambda c: len(c.galaxies)).galaxies)
+        galaxies = [cluster.galaxies for cluster in clusters]
+        vertex_points = [cluster.rotated_cube for cluster in clusters]
  
-        galaxies=concatenate( galaxies, axis=0 )
-        vertex_points=concatenate( vertex_points, axis=0 )
+        galaxies = concatenate( galaxies, axis = 0 )
+        vertex_points = concatenate( vertex_points, axis = 0 )
  
-        center = galaxies[:,:3].mean(axis=0)
+        center = galaxies[:, :self.n_dim].mean(axis = 0)
  
-        projections = center * dot(galaxies[:,:3], transpose([center])) / dot(center, center)
+        projections = center * dot(galaxies[:, :self.n_dim], transpose([center])) / dot(center, center)
  
-        distances_on_line=norm(projections-center,axis=1)
-        vectors_from_line=galaxies[:,:3]-projections
- 
-        length = distances_on_line.max()
-        width=norm(vectors_from_line,axis=1).max()
- 
-        cube=self.unit_cube.copy()*[width*2+self.init_cluster_width,width*2+self.init_cluster_width,length*2+self.init_cluster_length]
- 
-        return Cluster(center,cube,galaxies,self.init_cluster_length)
- 
-    def compress_cluster(self,cluster):
- 
-        galaxies=cluster.galaxies
-        if(galaxies.ndim==1 ):
-            galaxies=array([galaxies])
-        vertex_points=cluster.rotated_cube
- 
-        center = galaxies[:,:3].mean(axis=0)
- 
-        projections = center * dot(galaxies[:,:3], transpose([center])) / dot(center, center)
- 
-        distances_on_line=norm(projections-center,axis=1)
-        vectors_from_line=galaxies[:,:3]-projections
+        distances_on_line = norm(projections-center, axis = 1)
+        vectors_from_line = galaxies[:, :self.n_dim]-projections
  
         length = distances_on_line.max()
-        width=norm(vectors_from_line,axis=1).max()
+        width = norm(vectors_from_line, axis = 1).max()
+
+        cube = n_dim_cube(self.n_dim, length*2+self.init_cluster_length, width*2+self.init_cluster_width)
  
-        cube=self.unit_cube.copy()*[width*2+self.init_cluster_width/10,width*2+self.init_cluster_width/10,length*2+self.init_cluster_length/10]
+        return Cluster(center, cube, galaxies, self.init_cluster_length, n_dim = self.n_dim, new_galaxiex_koef = len(galaxies)/max_cluster_size)
  
-        return Cluster(center,cube,galaxies,self.init_cluster_length)
+    def compress_cluster(self, cluster):
  
-    def step(self,grow_coef):
+        galaxies = cluster.galaxies
+        if(galaxies.ndim == 1 ):
+            galaxies = array([galaxies])
+        vertex_points = cluster.rotated_cube
  
-        is_done=True
+        center = galaxies[:, :self.n_dim].mean(axis = 0)
+ 
+        projections = center * dot(galaxies[:, :self.n_dim], transpose([center])) / dot(center, center)
+ 
+        distances_on_line = norm(projections-center, axis = 1)
+        vectors_from_line = galaxies[:, :self.n_dim]-projections
+
+        length = distances_on_line.max()
+        width = norm(vectors_from_line, axis = 1).max()
+ 
+        cube = n_dim_cube(self.n_dim, length*2+self.init_cluster_length/10, width*2+self.init_cluster_width/10)
+        
+        return Cluster(center, cube, galaxies, self.init_cluster_length, n_dim = self.n_dim)
+ 
+    def step(self, grow_coef):
+ 
+        is_done = True
         for cluster in self.clusters:
             if(not cluster.is_complete):
-                is_done=False
+                is_done = False
                 break
  
         if (is_done):
             return False
  
         for cluster in self.clusters:
-            cluster.grow(1+(grow_coef-1)/ (1 + cluster.times_grow/5 + len(cluster.galaxies)/10))
+            cluster.grow(grow_coef)
         self.merge()
         return True
  
-    def split_array(self,a, n):
+
+
+    def split_array(self, a, n):
         k, m = divmod(len(a), n)
         return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
  
     @ray.remote
-    def parallel_connectivity_matrix(self,clusters ,rows):
-        new_graph=zeros((len(rows),len(clusters)))
+    def parallel_connectivity_matrix(self, clusters , rows):
+        new_graph = zeros((len(rows), len(clusters)))
         for i in range(len(rows)):
-            for j in range(rows[i],len(clusters)):
-                cluster_j=clusters[j]
-                cluster_i=clusters[rows[i]]
-                if(norm(cluster_i.centroid-cluster_j.centroid)>(cluster_i.get_length()+cluster_j.get_length())/2):
+            for j in range(rows[i], len(clusters)):
+                cluster_j = clusters[j]
+                cluster_i = clusters[rows[i]]
+                if(norm(cluster_i.centroid-cluster_j.centroid) > (cluster_i.get_length()+cluster_j.get_length())/2):
                     continue 
  
-                if(np.arccos((dot(cluster_i.centroid, cluster_j.centroid) / (norm(cluster_i.centroid) * norm(cluster_j.centroid))))>self.limit_radian):
+                if(np.arccos((dot(cluster_i.centroid, cluster_j.centroid) / (norm(cluster_i.centroid) * norm(cluster_j.centroid)))) > self.limit_radian):
                     continue
  
-                if(self.check_collision(cluster_i,cluster_j)):
-                    new_graph[i,j]=1
+                if(self.check_collision(cluster_i, cluster_j)):
+                    new_graph[i, j] = 1
+                elif(self.check_collision(cluster_j, cluster_i)):
+                    new_graph[i, j] = 1
         return new_graph
  
-    def connectivity_matrix(self,clusters ,rows):
-        new_graph=zeros((len(rows),len(clusters)))
+    def connectivity_matrix(self, clusters , rows):
+        new_graph = zeros((len(rows), len(clusters)))
         for i in range(len(rows)):
-            for j in range(rows[i],len(clusters)):
-                cluster_j=clusters[j]
-                cluster_i=clusters[rows[i]]
-                if(norm(cluster_i.centroid-cluster_j.centroid)>(cluster_i.get_length()+cluster_j.get_length())):
+            for j in range(rows[i], len(clusters)):
+                cluster_j = clusters[j]
+                cluster_i = clusters[rows[i]]
+                if(norm(cluster_i.centroid-cluster_j.centroid) > (cluster_i.get_length()+cluster_j.get_length())):
                     continue 
  
-                if(np.arccos((dot(cluster_i.centroid, cluster_j.centroid) / (norm(cluster_i.centroid) * norm(cluster_j.centroid))))>self.limit_radian):
+                if(np.arccos((dot(cluster_i.centroid, cluster_j.centroid) / (norm(cluster_i.centroid) * norm(cluster_j.centroid)))) > self.limit_radian):
                     continue
  
-                if(self.check_collision(cluster_i,cluster_j)):
-                    new_graph[i,j]=1
+                if(self.check_collision(cluster_i, cluster_j)):
+                    new_graph[i, j] = 1
+                elif(self.check_collision(cluster_j, cluster_i)):
+                    new_graph[i, j] = 1
         return new_graph
-    def fit(self,data):
+    def fit(self, data):
         try:
             ray.init()
-            data_np=array(data)
-            self.clusters=[Cluster(append(data_np[i],i), init_length = self.init_cluster_length,  init_width = self.init_cluster_width) for i in range(len(data_np)) ]
-            iter_num=1
+            data_np = array(data)
+            self.n_dim = len(data_np[0])
+            self.clusters = [Cluster(append(data_np[i], i), init_length = self.init_cluster_length,  init_width = self.init_cluster_width, n_dim = self.n_dim) for i in range(len(data_np)) ]
+            iter_num = 1
             start = time.time()
             while(self.step(self.coefs[iter_num-1])):
-                print('iter : ',iter_num,', n_clusters: ', len(self.clusters),', time: ',time.time()-start,' s')
-                iter_num+=1
+                print('iter : ', iter_num, ', n_clusters: ', len(self.clusters), ', time: ', time.time()-start, ' s')
+                iter_num += 1
                 start = time.time()
-    
+
                 if(iter_num > self.max_iter):
                     break
-    
-            galaxies=[]
 
-            galaxies=[append(self.clusters[i].galaxies, ones((len(self.clusters[i].galaxies),1))*i , axis=1) for i in range(len(self.clusters))]
-            self.clusters=[self.compress_cluster(cluster) for cluster in self.clusters]
-            galaxies=concatenate(galaxies)
-            galaxies = galaxies[galaxies[:,3].argsort()]  
-            return galaxies[:,4]
+            galaxies = []
+
+            galaxies = [append(self.clusters[i].galaxies, ones((len(self.clusters[i].galaxies), 1))*i , axis = 1) for i in range(len(self.clusters))]
+            self.clusters = [self.compress_cluster(cluster) for cluster in self.clusters]
+            galaxies = concatenate(galaxies)
+            galaxies = galaxies[galaxies[:, self.n_dim].argsort()]  
+            return galaxies[:, -1]
         except Exception as e:
             print(e)
         finally:
