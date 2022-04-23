3a7a7cf4a9c038190603ed2fbb012d7a177876b5
Andrey Shan <avshan@edu.hse.ru>
2022-04-23 23:03:15 +0300

    add cython computation

--- pbgca/__init__.py
+++ pbgca/__init__.py
@@ -2,4 +2,7 @@ from pbgca.cluster import Cluster
 from pbgca.clusterer import Clusterer
 from pbgca.cube_generator import CubeGenerator
 from pbgca.connectivity_matrix import ConnectivityMatrix
+from pbgca.cm_python import connectivity_matrix_python
+from cm_cython import connectivity_matrix_cython
+
 from pbgca.plot_generator import get_clusters_plot
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages,  Extension
 
 # To use a consistent encoding
 from codecs import open
 from os import path
+
+import numpy
 
 # The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
@@ -12,6 +14,7 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
+
 setup(
     name='pbgca',
     packages=find_packages(include=['pbgca']),
@@ -38,5 +41,12 @@ setup(
         "Operating System :: OS Independent"
     ],
     include_package_data=True,
-    install_requires=["numpy","pandas","ray","scipy","plotly","sklearn"]
+    install_requires=["numpy","pandas","ray","scipy","plotly","sklearn"],
+    ext_modules=[
+        Extension(
+            'pbgca.cm__cython',
+            sources=['pbgca/cm_cython.pyx'],
+        ),
+    ],
+    include_dirs=[numpy.get_include()]
 )
