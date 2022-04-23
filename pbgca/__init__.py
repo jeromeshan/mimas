badfb823d07b27bef442e3cae68f3cb0753b9f9b
Andrey Shan <avshan@edu.hse.ru>
2022-04-24 00:36:19 +0300

    fix init with  pyximport

--- pbgca/__init__.py
+++ pbgca/__init__.py
@@ -3,6 +3,9 @@ from pbgca.clusterer import Clusterer
 from pbgca.cube_generator import CubeGenerator
 from pbgca.connectivity_matrix import ConnectivityMatrix
 from pbgca.cm_python import connectivity_matrix_python
-from cm_cython import connectivity_matrix_cython
+import pyximport
+pyximport.install(setup_args={"include_dirs":np.get_include()},
+                  reload_support=True)
+from pbgca.cm_cython import connectivity_matrix_cython
 
 from pbgca.plot_generator import get_clusters_plot
